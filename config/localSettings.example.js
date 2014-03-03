2760d36d56a8e7c2e5cd0841e50f6142a3cf02a1
Kenneth Kouot <kenneth@wikia-inc.com>
2014-03-02 20:16:59 -0800

    copy hapi boilerplate files from https://github.com/kenkouot/hapi-web-app-boilerplate

---
+++ config/localSettings.example.js
@@ -0,0 +1,11 @@
+/**
+ * localSettings for application
+ * @example
+ * var localConfig = require('./config').localSettings()
+ */
+module.exports = function() {
+	return {
+		host: 'localhost',
+		port: 8000
+	};
+};
---
+++ controllers/home/index.js
@@ -0,0 +1,6 @@
+exports.index = function( request, reply) {
+	reply.view( 'home/index', {
+		title: 'foo',
+		message: 'bar'
+	});
+};
